22a30d19cab3dcde3ba09fd5e8dcf7c9c7021225
Hassan Gaber <36974406+hassangaber@users.noreply.github.com>
2020-08-30 16:33:44 -0400

    Create DNSaddress.go

---
+++ diagnostictools/DNSaddress.go
@@ -0,0 +1,24 @@
+//Go Algorithm developed for fetching DNS address of given URLs
+//Implementation 2 for Network Dynamics
+
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+
+	fmt.Println("Enter URL:")
+	var IP string
+	fmt.Scanf("%s", &IP)
+	fmt.Println("\n URL resolves to:")
+	fmt.Println(dnsLookup(IP))
+
+}
+
+func dnsLookup(ipStr string) (string, error) {
+	ipAddr, err := net.ResolveIPAddr("ip", ipStr)
+	return ipAddr.String(), err
+}
